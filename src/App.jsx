4d2da5386b02a9ec58640939bc79a4ded6f08092
Mextli <luna.vazquezv@gmail.com>
2018-12-04 03:54:01 -0600

    se realiza el dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from './firebase';
 import './App.css';
+import { Table } from 'react-materialize'
 
 class App extends Component {
   state = {
@@ -18,7 +19,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        firebase
+
+        <header className="title-broxel">
+          Broxel
+        </header>
+        <section>
+          <Table>
+            <tbody>
+              <tr>
+                <th data-field>ID</th>
+                <th data-fiel>Estado</th>
+                <th data-fiel>Nombre</th>
+                <th data-fiel>Fecha</th>
+                <th data-fiel>Monto</th>
+                <th data-fiel>Dirección</th>
+                <th data-fiel>Documentos</th>
+              </tr>
+              {this.state.solicitudes.map(element => {
+                return (
+                  <tr key={element.numeroSolicitud}>
+                    <td>{element.numeroSolicitud}</td>
+                    <td>{element.status}</td>
+                    <td>{element.nombre}</td>
+                    <td>{element.fecha}</td>
+                    <td>{element.monto}</td>
+                    <td>{element.direccion}</td>
+                    <td>Ver</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </section>
       </div>
     );
   }
